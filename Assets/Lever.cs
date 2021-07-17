750076108fd2e5d2f338e391c9b0f55999cd21d5
Darrylchoo <darrylchoo01@gmail.com>
2021-07-17 18:49:04 +0800

    Lever + Map Design
    - Change key bindings
    - Add lever
    - Add lights at bottom
    - Increase moving platform speed

---
+++ Assets/Lever.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ButtonMode
+{
+    STEP,
+    STAY
+}
+
+public class Lever : Trigger
+{
+    [SerializeField] private ButtonMode mode;
+    [SerializeField] private SpriteRenderer s_lever;
+    [SerializeField] private Sprite s_leverOn;
+    [SerializeField] private Sprite s_leverOff;
+
+    public bool triggered;
+
+    private void ChangeSprite()
+    {
+        if (!triggered)
+        {
+            triggered = true;
+            s_lever.sprite = s_leverOn;
+        }
+        else
+        {
+            triggered = false;
+            s_lever.sprite = s_leverOff;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isServer) RpcFireTrigger();
+
+        ChangeSprite();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.parent.CompareTag("Player"))
+        {
+            if (mode == ButtonMode.STAY)
+            {
+                if (isServer) RpcFireTrigger();
+            }
+        }
+    }
+}
